95d262e6384ae84d7f9267e3ddbfcd0c059545fb
Ronaldo-Akira <ronaki.kinoshita@gmail.com>
2024-12-01 20:44:09 -0300

    part 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,35 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val left = mutableListOf<Int>()
+        val right =  mutableListOf<Int>()
+
+        input.forEach {
+            val line = it.split("\\s+".toRegex())
+            left.add(line[0].toInt())
+            right.add(line[1].toInt())
+        }
+
+        val leftSorted = left.sorted()
+        val rightSorted = right.sorted()
+
+        val pairs: List<Pair<Int, Int>> = leftSorted.zip(rightSorted)
+
+        var sum = 0
+        pairs.forEach {pair ->
+            sum += abs(pair.first - pair.second)
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
